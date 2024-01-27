c2655f35e72a4d4c5b29d49dfb68ac43aafdbc99
Powell <powellfendley@gmail.com>
2024-01-26 17:00:44 -0700

    Updates to readme and extra changes.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# beaker-elwood
+# Beaker Climate Data Operators
+
+Beaker Climate Data Operators is a Beaker module designed to handle geographical/climate data. It provides a set of tools for detecting the resolution of a dataset, which can be particularly useful when regridding a dataset or when the resolution is unknown.
+
+## Startup
+
+To use Beaker Climate Data Operators, follow these steps:
+
+1. Navigate to the project directory: `cd beaker-climate-data-operators`
+2. Create a `.env` file and fill it out with the necessary environment variables. Refer to the `envfile.sample` file for the required variables.
+3. Launch the container using Docker Compose: `docker-compose up -d`
+4. Access the Beaker Climate Data Operations application at `http://localhost:8888/dev_ui?`
+
+## Usage
+
+Once in the user interface, you can use the notebook like a regular Jupyter notebook. There is also an LLM assistant you can use to access special features in the notebook.
+
+You can request the LLM to retrieve a dataset from the HMI server. You will need to provide a uuid and a filename.
+
+You can request the LLM to provide you plotting code in order to preview netcdf files.
+The LLM will ask for a variable name in the notebook, and if you have any particular geographical column names, a data variable name, and a time slice index.
+There are defaults for latter 3 values.
+
+You can request the LLM to provide regridding code in order to regrid a netcdf dataset.
+
+You can also request the LLM save a dataset to the HMI server. This will take a dataset variable from the notebook and persist it to the HMI.
+
--- src/beaker_elwood_context/context.py
+++ src/beaker_elwood_context/context.py
@@ -29,46 +29,15 @@ class ElwoodContext(BaseContext):
 
     async def auto_context(self):
         intro = f"""
-You are python software engineer whose goal is to help with dataset manipulation in {self.metadata.get("name", "a Jupyter notebook")}.
+    You are a software engineer working on a climate dataset operations tool in a Jupyter notebook.
 
-You are working with the following types of dataset(s):
-CSV
-NetCDF
-GeoTIFF
-Excel
+    Your goal is to help users perform various operations on climate datasets, such as regridding NetCDF datasets and plotting/previewing NetCDF files. 
+    Additionally, the tools provide functionality to retrieve datasets from a storage server.
 
-You have access to the python library called elwood. This library is a data manipulation library. It has access to the following functions:
-{await self.get_available_functions()}
-"""
-        outro = f"""
-Please answer any user queries to the best of your ability, but do not guess if you are not sure of an answer.
-If you are asked to manipulate or visualize the dataset.
-"""
+    Please provide assistance to users with their queries related to climate dataset operations.
 
-        result = "\n".join([intro, outro])
+    Remember to provide accurate information and avoid guessing if you are unsure of an answer.
+    """
+
+        result = intro
         return result
-
-    async def get_available_functions(self, parent_header={}):
-        """
-        This function should be used to discover the available functions in the elwood library and get an object containing their docstrings so you can figure out how to use them.
-
-        This function will return an object and store it into self.elwood_functions. The object will be a dictionary with the following structure:
-        {
-           function_name: <function docstring>,
-           ...
-        }
-
-        Args:
-            parent_header (dict, optional): Not used currently. Defaults to {}.
-        """
-        code = self.get_code("elwood_info")
-        elwood_info_response = await self.beaker_kernel.evaluate(
-            code,
-            parent_header=parent_header,
-        )
-        elwood_info = elwood_info_response.get("return")
-        for var_name, info in elwood_info.items():
-            if var_name in self.elwood__functions:
-                self.elwood__functions[var_name] = info
-            else:
-                self.elwood__functions[var_name] = info
